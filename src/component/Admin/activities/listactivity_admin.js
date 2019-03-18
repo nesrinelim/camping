46f0f9e375d7651a8c1ae4e06b4da0fb96a09241
halima91 <belhajali.halima@gmail.com>
2019-03-18 11:57:02 +0100

    halimacommitj18locnew

--- src/component/Admin/activities/listactivity_admin.js
+++ src/component/Admin/activities/listactivity_admin.js
@@ -12,10 +12,15 @@ class Item extends Component {
     render() { 
         const {item} = this.props
         return ( <div style={{display :'flex' ,width: '61%' , marginLeft: "20%",justifyContent:'space-between'}}>
+        <div style={{display :'flex',justifyContent:'space-between' ,width:'50%'}}>
             <h5 >{item.typeAct} </h5>
             <h5>{item.datein}</h5>
-           <Link to={`/admin1/edit/${item._id}`} > <Button>Edit</Button></Link>
-            <Button>Delete</Button>
+            </div>
+            <div>
+           <Link to={`/admin1/edit/${item._id}`} > <i style={{color:"grey"}}class="material-icons">edit</i></Link>
+           <i  style={{color:"red"}} class="material-icons">delete</i>
+           <i class="material-icons">sync_disabled</i>
+           </div>
         </div> );
     }
 }
