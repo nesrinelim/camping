2bb354eaff4d5ceca4c4a3e48326af40ffbccdcd
nesrinelim <nesrinlimayem@gmail.com>
2019-03-19 17:01:43 +0100

    commit19

--- src/component/Admin/activities/listactivity_admin.js
+++ src/component/Admin/activities/listactivity_admin.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import Item from './itemactivity_admin'
 import axios from 'axios';
-<<<<<<< HEAD
-
-import Item from './itemactivity_admin'
 
 
 
-=======
->>>>>>> origin/halima
+
+
 
 class Listactivity extends Component {
     constructor(props) {
@@ -19,23 +16,15 @@ class Listactivity extends Component {
     componentDidMount(){
         axios.get('/activitieslist')
         .then(res=>{this.setState({tab1 : res.data})})
-<<<<<<< HEAD
         console.log('component did mount')
     }
     componentDidUpdate(){
         axios.get('/activitieslist')
         .then(res=>{this.setState({tab1 : res.data})})
         console.log('component updated')
-=======
       
     }
-    componentDidUpdate(){
-
-        axios.get('/activitieslist')
-        .then(res=>{this.setState({tab1 : res.data})})
-        
->>>>>>> origin/halima
-    }
+   
     render() { 
    
         return ( <div>
@@ -50,8 +39,4 @@ class Listactivity extends Component {
 
 
  
-<<<<<<< HEAD
 export default Listactivity;
-=======
-export default  (Listactivity);
->>>>>>> origin/halima
